d55c7880b284b5e70d890c2a84ef8f664ae0be11
oscarforri <oscarforri@gmail.com>
2016-05-25 18:34:45 +0000

    S

--- webapp.py
+++ webapp.py
@@ -7,6 +7,30 @@ from sqlalchemy_declarative import User, Base
 
 app = Flask(__name__)
 
+def get_user():
+    engine = create_engine('sqlite:///sqlalchemy_database.db', echo=True)
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    data = session.query(User.username, User.userid, User.email, User.realname, User.amount).all()
+    return data
+
+def save_user(username, userid, realname, email):
+    engine = create_engine('sqlite:///sqlalchemy_database.db')
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    ed_user = User(username, userid, realname, email, amount=0.0)
+    session.add(ed_user)
+    session.commit()
+
+def delete_user():
+    pass
+
+def update_user():
+    engine = create_engine('sqlite:///sqlalchemy_database.db')
+    DBSession = sessionmaker(bind=engine)
+    ses
+
+
 
 #Home Page.
 @app.route('/')
@@ -19,27 +43,33 @@ def users_management():
     return render_template('users_management.html')
 
 #Gestio d'usuaris (CREATE).
-@app.route('/users_management/create_user', methods=['GET','POST'])
+@app.route('/create_user', methods=['GET','POST'])
 def create_user():
     if request.method == 'GET':
         return render_template('create_user.html')
     elif request.method == 'POST':
-	#username = request.form.get('username')
-	#userid = request.form.get('userid')
-        #realname = request.form.get('realname')
-	#email = request.form.get('email')
-	#save_userORM(username,fullname,email,password)
-	return render_template('OK')
+	username = request.form.get('username')
+	userid = request.form.get('userid')
+        realname = request.form.get('realname')
+	email = request.form.get('email')
+	save_user(username, userid, realname, email)
+	return 'OK'
 
 #Gestio d'usuaris (DELETE).
-@app.route('/users_management/delete_user')
+@app.route('/users_management/delete_user', methods=['GET','POST'])
 def delete_user():
-    return render_template('delete_user.html')
+    if request.method == 'GET':
+	 return render_template('delete_user.html')
+    elif request.method == 'POST':
+	 return "Delete user correctly?"
 
 #Gestio d'usuaris (UPDATE).
-@app.route('/update_user')
+@app.route('/users_management/update_user', methods=['GET','POST'])
 def update_user():
-    return render_template('update_user.html')
+    if request.method == 'GET':
+	return render_template('update_user.html')
+    elif request.method == 'POST':
+	return "Update user correctly?"
 
 #Gestio de surtidors
 @app.route('/kegs_management')
@@ -47,14 +77,15 @@ def kegs_management():
     return render_template('kegs_management.html')
 
 #Mostrar tots els usuaris.
-@app.route('/show_users')
-def show_users():
+@app.route('/show_')
+def show_():
     return "Mostrar tots els usuaris"
 
 #Mostrar tots els surtidors.
-@app.route('/show_kegs')
-def show_kegs():
-    return "Mostrar tots els sortidors"
+@app.route('/show_users')
+def show_users():
+    data = get_user()
+    return render_template('show_user_table.html', data=data)
 
 
 if __name__ == '__main__':
