85336246ab18d23c90a64f999c17d3a297607ff6
oscarforri <oscarforri@gmail.com>
2016-06-14 14:37:18 +0000

    Added __json__ to sqlalchemy_declarative

--- sqlalchemy_declarative.py
+++ sqlalchemy_declarative.py
@@ -24,6 +24,16 @@ class User(Base):
         return "<User(username='%s', userid='%s', realname='%s', email='%s', amount='%d')>" % (
                         self.username, self.userid, self.realname, self.email, self.amount)
 
+    def __json__ (self):
+	dic_user = {}
+	dic_user["id"] = self.id
+	dic_user["username"] = self.username
+ 	dic_user["userid"] = self.userid
+	dic_user["realname"] = self.realname
+	dic_user["email"] = self.realname
+	dic_user["amount"] = self.amount
+	return dic_user
+
 class Keg(Base):
     __tablename__ = 'keg'
     id = Column(Integer, primary_key=True)
@@ -37,6 +47,12 @@ class Keg(Base):
     def __repr__(self):
         return "<Keg(kegid='%s',amount='%d')>" % (
                         self.kegid, self.amount)
+    def __json__ (self):
+        dic_keg = {}
+        dic_keg["id"] = self.id
+        dic_keg["kegid"] = self.kegid
+        dic_keg["amount"] = self.amount
+        return dic_keg
 
 engine = create_engine('sqlite:///sqlalchemy_database.db')
 Base.metadata.create_all(engine)
